b1a53e8ffce0ae3c52fa228508c782b07ceb146f
David Goudet <davidgoudet>
2019-10-07 21:42:00 -0400

    Retweets vs. Likes

---
+++ 05_graph_retweets_vs_likes.py
@@ -0,0 +1,17 @@
+#Importamos matploblib, que nos permitirá hacer el gráfico (Funciona en Repl.it)
+import matplotlib.pyplot as plt
+from twitterscraper import query_tweets_from_user
+
+retweets = []
+likes = []
+
+all_tweets = (query_tweets_from_user("barackobama", limit=100))
+
+for tweet in all_tweets:
+    retweets.append(tweet.retweets)
+    likes.append(tweet.likes)
+
+#Imprimimos un gráfico de retweets vs likes
+plt.plot(retweets, likes, 'o-')
+plt.show()
+plt.savefig("retweetslikes.png")
