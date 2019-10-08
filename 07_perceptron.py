f368d511f044574fdf73dd586025ffb057240c20
David Goudet <davidgoudet>
2019-10-07 21:54:41 -0400

    Perceptron

---
+++ 07_perceptron.py
@@ -0,0 +1,47 @@
+from twitterscraper import query_tweets_from_user
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.neural_network import MLPClassifier
+sid = SentimentIntensityAnalyzer()
+#Agregamos más iteraciones y hacemos mucho más grande la red
+model=MLPClassifier(hidden_layer_sizes=(150,100,100),max_iter=1000)
+
+features = []
+labels = []
+
+all_tweets = query_tweets_from_user("barackobama", limit=800)
+
+#Ahora entrenamos con 600 tweets
+training = all_tweets[:600]
+testing = all_tweets[600:]
+
+for tweet in training:
+	tweetAnalysis = sid.polarity_scores(tweet.text)
+	#Dividimos el número de retweets y likes entre 1000 y lo convertimos en entero, para que ahora 30.000 y 30.500 ambos sean 30
+	#Multiplicamos las probabilidades para trabajar con números más grandes, ahora 0.10 será 10
+	features.append([int(tweetAnalysis["neg"]*100),int(tweetAnalysis["pos"]*100),int(tweetAnalysis["neu"]*100)])
+	labels.append(int(tweet.likes/1000))
+
+model = model.fit(features,labels)
+
+matches = 0
+errors = 0
+for test in testing:	
+	print("Real:")
+	real = int(test.likes/1000)
+	print(real)
+	print("Prediction")
+	tweetAnalysis = sid.polarity_scores(test.text)
+	feature = [[int(tweetAnalysis["neg"]*100),int(tweetAnalysis["pos"]*100),int(tweetAnalysis["neu"]*100)]]
+	predict = model.predict(feature)
+	if real -1 <= predict <= real+1:
+		matches += 1
+	else:
+		errors += 1
+	
+
+print("matches")
+print(matches)
+print("errors")
+print(errors)
